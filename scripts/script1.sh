67cb2f28ab675263eb13fc95aa70cd365417b51b
Wenjuan <wenjuan.zhang66@gmail.com>
2021-03-15 13:21:45 -0600

    Move all scripts in scripts dir

---
+++ scripts/script1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now."
+
+echo "Hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+w
+echo
+#w is a command-line utility that displays information about currently logged in users and what each user is doing. It also gives information about how long the system has been running, the current time, and the system load average.
+
+echo "I'm setting two variables now."
+COLOUR="black"
+VALUE="9"
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
